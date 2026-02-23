48d9557c8cb0d1d638cdd507034fac391b78181d
Aslam Howlader <mdaslamhowlader09@gmail.com>
2026-02-23 10:43:15 -0600

    23-2-2026

---
+++ problem_Solve/src/Main.java
@@ -0,0 +1,24 @@
+
+interface Playable
+{
+    void play();
+}
+class Song implements Playable
+{
+    String title;
+   void play()
+   {
+
+   }
+
+}
+class  Video implements  Playable
+{
+    void play()
+    {
+
+    }
+}
+
+public class Interface {
+}
